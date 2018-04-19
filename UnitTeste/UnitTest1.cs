21da44d7fec9fae4f60fc89ee0c1acb3488d58fe
Gustavo <gustavo1071af@hotmail.com>
2018-04-18 23:59:18 -0300

    testes

---
+++ UnitTeste/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebServicesCorreios.Console;
+
+namespace UnitTeste
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TestOne()
+        {
+           
+            Assert.AreEqual("Rua Ministro Orozimbo Nonato", "Rua Ministro Orozimbo Nonato");
+        }
+
+        [TestMethod]
+        public void TestTwo()
+        {
+
+            Assert.AreEqual("Rua Ministro Orozimbo Nonato", "Rua Ministro Orozimbo");
+        }
+    }
+}
